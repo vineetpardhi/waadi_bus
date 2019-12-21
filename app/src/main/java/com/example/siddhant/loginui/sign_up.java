1d07b953a4c868ae6f058631e475ef7348c96240
RahulSolanki <rahulsolankib@gmail.com>
2019-12-21 11:31:07 +0530

    New PHONE VER

--- app/src/main/java/com/example/siddhant/loginui/sign_up.java
+++ app/src/main/java/com/example/siddhant/loginui/sign_up.java
@@ -1,5 +1,13 @@
 package com.example.siddhant.loginui;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
@@ -29,6 +37,7 @@ public class sign_up extends AppCompatActivity {
     EditText username,password,cpassword,email,verfication,otp;
     Button button;
     member member;
+    String codeSent;
     DatabaseReference reff;
     ProgressDialog pro;
     private String verificationid;
@@ -38,6 +47,9 @@ public class sign_up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        mAuth = FirebaseAuth.getInstance();
+
+
         otp = (EditText) findViewById(R.id.otp);
         username = (EditText) findViewById(R.id.username);
         password = (EditText) findViewById(R.id.password);
@@ -45,29 +57,33 @@ public class sign_up extends AppCompatActivity {
         email = (EditText) findViewById(R.id.email);
         verfication = (EditText) findViewById(R.id.verfication);
         button = (Button) findViewById(R.id.button);
+
+
+        findViewById(R.id.button13).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sendVerificationCode();
+            }
+        });
+        findViewById(R.id.signup).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                verifySignInCode();
+            }
+        });
         pro = new ProgressDialog(this);
         member = new member();
         reff = FirebaseDatabase.getInstance().getReference().child("member");
         final String phonenumber = verfication.getText().toString();
-        findViewById(R.id.button13).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-                    String number = verfication.getText().toString().trim();
-
-                    if(number.isEmpty() || number.length() < 10) {
-                        verfication.setError("Valid number is required");
-                        verfication.requestFocus();
-                        return;
-                    }
-                    sendVerificationCode("+91"+number);
-
-            }
-        });
         button.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
+                String Cpassword = cpassword.getText().toString();
+                String Password = password.getText().toString();
+
+
                 String user = username.getText().toString();
                 if (email.getText().toString().trim().isEmpty()) {
                     email.setError("email field can't be empty!");
@@ -79,7 +95,7 @@ public class sign_up extends AppCompatActivity {
                     password.setError("Password field cannot be empty");
                 } else if (cpassword.getText().toString().isEmpty()) {
                     cpassword.setError("Confirm password field cannot be empty!");
-                } else if (cpassword.getText().toString() != password.getText().toString()) {
+                } else if (!Password.equals(Cpassword)) {
                     cpassword.setError("Password do not match!");
                 } else {
                     pro.setMessage("Registering...");
@@ -91,94 +107,85 @@ public class sign_up extends AppCompatActivity {
                     member.setVerification(verfication.getText().toString().trim());
                     reff.child(username.getText().toString().trim()).setValue(member);
                     Toast.makeText(sign_up.this, "Successfully Signed in", Toast.LENGTH_LONG).show();
-                    startActivity(new Intent(getApplicationContext(), login.class));
+                    //startActivity(new Intent(getApplicationContext(), login.class));
                 }
-            }
-        });
-
-
-        findViewById(R.id.signup).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                String code = otp.getText().toString().trim();
-
-                if ((code.isEmpty() || code.length() < 6)) {
-
-                    otp.setError("Enter code...");
-                    otp.requestFocus();
-                    return;
-                }
-                verifyCode(code);
-
             }
         });
     }
-        private void verifyCode(String code){
-            PhoneAuthCredential credential = PhoneAuthProvider.getCredential(verificationid, code);
-            signInWithCredential(credential);
+        private void verifySignInCode(){
+            String code = otp.getText().toString();
+            PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codeSent, code);
+            signInWithPhoneAuthCredential(credential);
         }
 
-        private void signInWithCredential(PhoneAuthCredential credential) {
+        private void signInWithPhoneAuthCredential(PhoneAuthCredential credential) {
             mAuth.signInWithCredential(credential)
-                    .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                         @Override
                         public void onComplete(@NonNull Task<AuthResult> task) {
                             if (task.isSuccessful()) {
-
-                                Intent intent = new Intent(sign_up.this, MainActivity.class);
-                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-
-                                startActivity(intent);
-
+                                //here you can open new activity
+                                Toast.makeText(getApplicationContext(),
+                                        "Login Successfull", Toast.LENGTH_LONG).show();
                             } else {
-                                Toast.makeText(sign_up.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                    Toast.makeText(getApplicationContext(),
+                                            "Incorrect Verification Code ", Toast.LENGTH_LONG).show();
+                                }
                             }
                         }
-
                     });
         }
 
-        private void sendVerificationCode(String number){
-            Toast.makeText(sign_up.this,number, Toast.LENGTH_LONG).show();
+        private void sendVerificationCode(){
+
+            String phone = verfication.getText().toString();
+
+            if(phone.isEmpty()){
+                verfication.setError("Phone number is required");
+                verfication.requestFocus();
+                return;
+            }
+
+            if(phone.length() < 10 ){
+                verfication.setError("Please enter a valid phone");
+                verfication.requestFocus();
+                return;
+            }
+
 
             PhoneAuthProvider.getInstance().verifyPhoneNumber(
-                    number,
-                    60,
-                    TimeUnit.SECONDS,
-                    TaskExecutors.MAIN_THREAD,
-                    mCallBack
-            );
+
+                    phone,        // Phone number to verify
+                    60,                 // Timeout duration
+                    TimeUnit.SECONDS,   // Unit of timeout
+                    this,               // Activity (for callback binding)
+                    mCallbacks);        // OnVerificationStateChangedCallbacks
+            Toast.makeText(getApplicationContext(),
+                    "code sending ... ", Toast.LENGTH_LONG).show();
         }
 
-        private PhoneAuthProvider.OnVerificationStateChangedCallbacks
-                mCallBack = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
 
-            @Override
-            public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
-                super.onCodeSent(s, forceResendingToken);
-                Toast.makeText(sign_up.this, "code sent", Toast.LENGTH_LONG).show();
-                verificationid = s;
-            }
+
+        PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
 
             @Override
             public void onVerificationCompleted(PhoneAuthCredential phoneAuthCredential) {
-                String code = phoneAuthCredential.getSmsCode();
-                if (code != null){
-                    progressBar.setVisibility(View.VISIBLE);
-                    verifyCode(code);
-                }
+
             }
 
             @Override
             public void onVerificationFailed(FirebaseException e) {
-                Toast.makeText(sign_up.this, e.getMessage(),Toast.LENGTH_LONG).show();
 
             }
-        };
 
-    public void gotologin(View view) {
-        startActivity(new Intent(getApplicationContext(),login.class));
-    }
+            @Override
+            public void onCodeSent(String s, PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                super.onCodeSent(s, forceResendingToken);
+                Toast.makeText(getApplicationContext(),
+                        "code sent", Toast.LENGTH_LONG).show();
+                codeSent = s;
+            }
+        };
 
 }
