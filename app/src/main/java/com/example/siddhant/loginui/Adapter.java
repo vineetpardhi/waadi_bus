6930b48b54ceb11cc7320defd19edcb4d1a6882d
RahulSolanki <rahulsolankib@gmail.com>
2019-12-27 14:50:14 +0530

    rickshaw list done

--- app/src/main/java/com/example/siddhant/loginui/Adapter.java
+++ app/src/main/java/com/example/siddhant/loginui/Adapter.java
@@ -12,17 +12,44 @@ import android.widget.TextView;
 
 public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
     private newdriver[] mDataset;
+    private  OnItemClickListner mlistner;
 
+    public interface OnItemClickListner{
+        void onButtonClick(int position);
+    }
+
+    public void setOnClickListnerItem(OnItemClickListner listner)
+    {
+        this.mlistner=listner;
+    }
     public static class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView title;
         public TextView rno;
         public TextView phone;
+        public Button callbtn;
 
-        public MyViewHolder(View v) {
+        public MyViewHolder(View v, final OnItemClickListner listner) {
             super(v);
             rno = (TextView) v.findViewById(R.id.rno);
             phone = (TextView)v.findViewById(R.id.phone);
             title=(TextView) v.findViewById(R.id.title);
+            callbtn=(Button) v.findViewById(R.id.button16);
+
+            callbtn.setOnClickListener(new View.OnClickListener(){
+
+                @Override
+                public void onClick(View view) {
+                        if(listner!=null)
+                        {
+                            int position=getAdapterPosition();
+                            if(position!=RecyclerView.NO_POSITION)
+                            {
+                                listner.onButtonClick(position);
+                            }
+                        }
+                }
+            });
+
         }
     }
 
@@ -35,7 +62,7 @@ public class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
                                                      int viewType) {
     LayoutInflater inflater=LayoutInflater.from(parent.getContext());
     View view=inflater.inflate(R.layout.activity_list_item_layout,parent,false);
-        return new MyViewHolder(view);
+        return new MyViewHolder(view,mlistner);
     }
 
     @Override
--- app/src/main/java/com/example/siddhant/loginui/rikshaw_list.java
+++ app/src/main/java/com/example/siddhant/loginui/rikshaw_list.java
@@ -1,13 +1,18 @@
 package com.example.siddhant.loginui;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -32,32 +37,29 @@ public class rikshaw_list extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rikshaw_list);
-        acount=0;
+        acount = 0;
         final Bundle b = getIntent().getExtras();
 
         final String src = (b.getString("src")).toLowerCase();
         final String dest = (b.getString("dest")).toLowerCase();
-        final RecyclerView list=findViewById(R.id.lists);
+        final Adapter[] mAdapter = new Adapter[1];
+        final RecyclerView list = findViewById(R.id.lists);
         list.setLayoutManager(new LinearLayoutManager(this));
         reff = FirebaseDatabase.getInstance().getReference().child("member");
         reff.addListenerForSingleValueEvent(new ValueEventListener() {
 
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                for(DataSnapshot listsnapshot:dataSnapshot.getChildren())
-                {
-                    driver1=new ArrayList<newdriver>();
-                    String cust=listsnapshot.child("role").getValue(String.class);
-                    if(!cust.isEmpty()&&cust.equals("Driver"))
-                    {
-                        String a=listsnapshot.child("working").getValue().toString().toLowerCase();
-                        if(a.equals(src))
-                        {
-                            String b=listsnapshot.child("places").getValue().toString().toLowerCase();
-                            if(b.contains(dest))
-                            {
-                                Map<String,String> td=(HashMap<String, String>)listsnapshot.getValue();
-                                newdriver nd=new newdriver();
+                for (DataSnapshot listsnapshot : dataSnapshot.getChildren()) {
+                    driver1 = new ArrayList<newdriver>();
+                    String cust = listsnapshot.child("role").getValue(String.class);
+                    if (!cust.isEmpty() && cust.equals("Driver")) {
+                        String a = listsnapshot.child("working").getValue().toString().toLowerCase();
+                        if (a.equals(src)) {
+                            String b = listsnapshot.child("places").getValue().toString().toLowerCase();
+                            if (b.contains(dest)) {
+                                Map<String, String> td = (HashMap<String, String>) listsnapshot.getValue();
+                                newdriver nd = new newdriver();
                                 nd.setName(td.get("name"));
                                 nd.setMobileno(td.get("mobileno"));
                                 nd.setRno(td.get("rickshawno"));
@@ -66,20 +68,16 @@ public class rikshaw_list extends AppCompatActivity {
                         }
                     }
                 }
-                driver2=new ArrayList<newdriver>();
-                for(DataSnapshot listsnapshot:dataSnapshot.getChildren())
-                {
-                    String cust=listsnapshot.child("role").getValue(String.class);
-                    if(!cust.isEmpty()&&cust.equals("Driver"))
-                    {
-                        String a=listsnapshot.child("places").getValue().toString().toLowerCase();
-                        if(a.contains(src))
-                        {
-                            String b=listsnapshot.child("places").getValue().toString().toLowerCase();
-                            if(b.contains(dest))
-                            {
-                                Map<String,String> td=(HashMap<String, String>)listsnapshot.getValue();
-                                newdriver nd=new newdriver();
+                driver2 = new ArrayList<newdriver>();
+                for (DataSnapshot listsnapshot : dataSnapshot.getChildren()) {
+                    String cust = listsnapshot.child("role").getValue(String.class);
+                    if (!cust.isEmpty() && cust.equals("Driver")) {
+                        String a = listsnapshot.child("places").getValue().toString().toLowerCase();
+                        if (a.contains(src)) {
+                            String b = listsnapshot.child("places").getValue().toString().toLowerCase();
+                            if (b.contains(dest)) {
+                                Map<String, String> td = (HashMap<String, String>) listsnapshot.getValue();
+                                newdriver nd = new newdriver();
                                 nd.setName(td.get("name"));
                                 nd.setMobileno(td.get("mobileno"));
                                 nd.setRno(td.get("rickshawno"));
@@ -88,20 +86,17 @@ public class rikshaw_list extends AppCompatActivity {
                         }
                     }
                 }
-                driver3=new ArrayList<newdriver>();
-                for(DataSnapshot listsnapshot:dataSnapshot.getChildren())
-                {
-                    String cust=listsnapshot.child("role").getValue(String.class);
-                    if(!cust.isEmpty()&&cust.equals("Driver"))
-                    {
-                        String a=listsnapshot.child("working").getValue().toString().toLowerCase();
-                        if(a.equals(dest))
-                        {
-                            String b=listsnapshot.child("places").getValue().toString().toLowerCase();
-                            if(b.contains(src))
-                            {
-                                Map<String,String> td=(HashMap<String, String>)listsnapshot.getValue();
-                                newdriver nd=new newdriver();
+                driver3 = new ArrayList<newdriver>();
+                for (DataSnapshot listsnapshot : dataSnapshot.getChildren()) {
+                    String cust = listsnapshot.child("role").getValue(String.class);
+                    if (!cust.isEmpty() && cust.equals("Driver")) {
+                        String a = listsnapshot.child("working").getValue().toString().toLowerCase();
+                        if (a.equals(dest)) {
+                            String b = listsnapshot.child("places").getValue().toString().toLowerCase();
+                            if (b.contains(src)) {
+                                Map<String, String> td = (HashMap<String, String>) listsnapshot.getValue();
+                                newdriver nd = new newdriver();
+                                Toast.makeText(getApplicationContext(), td.toString(), Toast.LENGTH_SHORT).show();
                                 nd.setName(td.get("name"));
                                 nd.setMobileno(td.get("mobileno"));
                                 nd.setRno(td.get("rickshawno"));
@@ -110,8 +105,8 @@ public class rikshaw_list extends AppCompatActivity {
                         }
                     }
                 }
-                if(driver1.size()+driver2.size()+driver3.size()>0) {
-                    newdriver ng[] = new newdriver[driver1.size() + driver2.size() + driver3.size()];
+                if (driver1.size() + driver2.size() + driver3.size() > 0) {
+                    final newdriver ng[] = new newdriver[driver1.size() + driver2.size() + driver3.size()];
                     for (newdriver m : driver1) {
                         ng[acount] = new newdriver();
                         ng[acount] = m;
@@ -129,19 +124,45 @@ public class rikshaw_list extends AppCompatActivity {
                         ng[acount] = m;
                         acount = acount + 1;
                     }
-                    list.setAdapter(new Adapter(ng));
-                }
-                else
-                {
+                    mAdapter[0] = new Adapter(ng);
+                    list.setAdapter(mAdapter[0]);
+                    mAdapter[0].setOnClickListnerItem(new Adapter.OnItemClickListner() {
+                        @Override
+                        public void onButtonClick(int position) {
+                            callDriver(ng[position]);
+                        }
+                    });
+                } else {
 
 
                 }
+
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError databaseError) {
 
             }
         });
+    }
+
+    private void callDriver(newdriver newdriver) {
+        String s = "tel:" + newdriver.getMobileno();
+        Intent intent = new Intent(Intent.ACTION_CALL);
+        intent.setData(Uri.parse(s));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        Toast.makeText(getApplicationContext(), newdriver.getMobileno(), Toast.LENGTH_SHORT).show();
+
+        startActivity(intent);
     }
 
 }
